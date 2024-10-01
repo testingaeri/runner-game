d8ddfe5f56d5dbc1d65aab1193ae537c800693b9
testing <120217617+testingaeri@users.noreply.github.com>
2024-10-01 08:53:18 -0400

    Create Score.js

---
+++ backend/models/Score.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const scoreSchema = new mongoose.Schema({
+    username: String,
+    score: Number,
+});
+
+module.exports = mongoose.model('Score', scoreSchema);
